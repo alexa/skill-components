5bcf0f8885eaa8242911395a5b803c1e2b75691d
Karan Gandhi <karangg@amazon.com>
2023-03-21 02:18:20 +0530

    Modiying namespace in Acc linking with standardized version

--- account-linking/lambda/constants.ts
+++ account-linking/lambda/constants.ts
@@ -5,7 +5,7 @@ export class Constants {
     ***************************************************/
 
     // Skill Request Type Constants
-    static readonly IS_ACCOUNT_LINKED_API = 'com.amazon.alexa.accountlinking.required.isAccountLinked';
+    static readonly IS_ACCOUNT_LINKED_API = 'com.amazon.alexa.skill.components.account_linking.required.isAccountLinked';
     static readonly LINKED_STATUS = 'linked';
     static readonly UNLINKED_STATUS = 'unlinked';
     static readonly DIALOG_API_REQUEST = 'Dialog.API.Invoked';
